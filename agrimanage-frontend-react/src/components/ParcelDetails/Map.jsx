430d4d84d5c1a2877731b7f41481fd00570bf759
Bojana Živanić <zivanic.pr122.2019@uns.ac.rs>
2023-10-24 14:57:23 +0200

    Changed map component

--- agrimanage-frontend-react/src/components/ParcelDetails/Map.jsx
+++ agrimanage-frontend-react/src/components/ParcelDetails/Map.jsx
@@ -1,15 +1,19 @@
-import { MapContainer, Polygon, TileLayer } from "react-leaflet";
+import React, { useEffect, useRef } from 'react';
+import 'leaflet/dist/leaflet.css'; 
+import L from 'leaflet';
 
 const centerPosition = [45.25472833688446, 19.83317432993583];
 
-const Map = ({coordinates}) => {
+const Map = ({ coordinates }) => {
+  const mapRef = useRef(null);
 
-  return (   
-      <MapContainer center={centerPosition} zoom={35} scrollWheelZoom={true} >
-        <TileLayer url={import.meta.env.VITE_LINK_API} />
-       <Polygon positions={coordinates} color="blue" />
-      </MapContainer>   
-  );
+  useEffect(() => {
+    const map = L.map(mapRef.current).setView(centerPosition, 13);
+    L.tileLayer(import.meta.env.VITE_LINK_API).addTo(map);
+    L.polygon(coordinates, { color: 'blue' }).addTo(map);
+  }, [coordinates]);
+
+  return <div ref={mapRef} style={{ width: '100%', height: '400px' }}></div>;
 };
 
 export default Map;
